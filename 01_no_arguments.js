97f839e2984095119bafabfc5e92f468262ebac0
jeffrey vierra <st.pious@gmail.com>
2017-08-07 12:54:21 -1000

    Declarative functions --> Arrow functions
    Converted declarative functions into Arrow functions.

--- 01_no_arguments.js
+++ 01_no_arguments.js
@@ -1,10 +1,45 @@
 const name = "Jeff";
+/*
 
+Function declaration 
 function sayName() {
     const message = "My name is " + name;
     console.log(message);
 }
 
+
+ Function expression 
+const sayName = function() {
+    const message = "My name is " + name;
+    console.log(message);
+}
+*/
+
+/* Arrow Function */
+const sayName = () => {
+    const message = "My name is " + name;
+    console.log(message);
+}
+
+
+
+sayName();
+/*
+
+ Function Declaration 
+
 function sayBye() {
   console.log("Bye " + name);  
 }
+*/
+
+
+/* Arrow Function */
+const sayBye = () =>  {
+  console.log("Bye " + name);  
+}
+
+
+sayBye();
+
+
